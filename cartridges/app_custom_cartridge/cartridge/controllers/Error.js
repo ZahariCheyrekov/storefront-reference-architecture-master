676a1b1ced30f041dea2922a61d5f793899e1721
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-11 12:24:08 +0200

    Implement error start controller

--- cartridges/app_custom_cartridge/cartridge/controllers/Error.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/Error.js
@@ -10,6 +10,35 @@ var server = require('server');
  * @function
  * @memberof Error
  * @param {renders} - isml
+ * @param {serverfunction} - get/post
+ */
+server.use('Start', function (req, res, next) {
+    var URLUtils = require('dw/web/URLUtils');
+    var errorText = req.error;
+
+    if (errorText === 'Secure connection required for this request.' &&
+        !req.httpSecure &&
+        (req.httpHeaders.containsKey('x-is-request_method')) &&
+        (req.httpHeaders['x-is-request_method'] === 'GET')) {
+        res.redirect(URLUtils.url('error'));
+    }
+
+    if (req.getHttpHeaders().get("x-requested-with") != null &&
+        req.getHttpHeaders().get("x-requested-with") === "XMLHttpRequest") {
+
+        if (req.httpHeaders['Content-Type'] === 'application/json') {
+            res.render('error/generalerrorjson');
+        }
+    }
+
+    res.render('error/generalerror')
+    next();
+});
+
+/**
+ * @function
+ * @memberof Error
+ * @param {renders} - isml
  * @param {serverfunction} - get
  */
 
