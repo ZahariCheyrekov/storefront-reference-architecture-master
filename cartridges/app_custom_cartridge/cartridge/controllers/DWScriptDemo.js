0311c73e2d7dee91541db12deec51d0a11244c5b
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-11 17:34:56 +0200

    Create dw script show

---
+++ cartridges/app_custom_cartridge/cartridge/controllers/DWScriptDemo.js
@@ -0,0 +1,35 @@
+'use strict';
+
+/**
+ * @namespace DWScriptDemo
+ */
+
+var server = require('server');
+var consentTracking = require('*/cartridge/scripts/middleware/consentTracking');
+var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
+var DWScriptModel = require('*/cartridge/models/dwscript');
+
+/**
+ * @name Base/DWScriptDemo
+ * @function
+ * @memberof DWScriptDemo
+ * @param {middleware} - consentTracking.consent
+ * @param {middleware} - cache.applyDefaultCache
+ * @param {category} - non-sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.get('Show', consentTracking.consent, function (req, res, next) {
+    var Site = require('dw/system/Site');
+    var pageMetaHelper = require('*/cartridge/scripts/helpers/pageMetaHelper');
+
+    pageMetaHelper.setPageMetaTags(req.pageMetaData, Site.current);
+
+    var dwSriptModel = new DWScriptModel(customer);
+
+    res.render('dwscriptdemo',dwSriptModel);
+    next();
+}, pageMetaData.computedPageMetaData);
+
+module.exports = server.exports();
