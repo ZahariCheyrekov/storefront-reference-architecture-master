01a3e8c237e2d9ffcf5db61882c9c33c3b7fc7db
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-12 15:24:58 +0200

    Implement get product by id functionality

--- cartridges/app_custom_cartridge/cartridge/models/dwscript.js
+++ cartridges/app_custom_cartridge/cartridge/models/dwscript.js
@@ -1,6 +1,33 @@
 'use strict';
 
 /**
+ * Function accepts productId and returns product by the given id
+ * @param {String} productId
+ * @returns {dw.catalog.Product | null}
+ */
+function getProductById(productId) {
+    var ProductMgr = require('dw/catalog/ProductMgr');
+    var product = ProductMgr.getProduct(productId);
+    return product;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**
  * @returns
  */
 function getCustomerID(customer) {
@@ -11,8 +38,10 @@ function getCustomerID(customer) {
  *
  * @param {*} customer
  */
-function DWScriptModel(customer) {
+function DWScriptModel(customer, productId, customerId) {
     this.ID = getCustomerID(customer);
+    this.product = getProductById(productId);
+    this.productCategory = getProductCategory(productId);
 }
 
 module.exports = DWScriptModel;
