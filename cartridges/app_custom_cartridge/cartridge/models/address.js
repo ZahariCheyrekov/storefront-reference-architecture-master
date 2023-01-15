b58cd71943fd34ed48e056eb04e47404a40a14c6
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-15 12:04:55 +0200

    Fix address construcrtor

--- cartridges/app_custom_cartridge/cartridge/models/address.js
+++ cartridges/app_custom_cartridge/cartridge/models/address.js
@@ -11,4 +11,25 @@ function address(object, demo_prop) {
     this.address = object;
 }
 
+'use strict';
+
+var base = module.superModule;
+var addressHelpers = require('*/cartridge/scripts/helpers/addressHelpers');
+
+/**
+
+function address(addressObject) {
+    base.call(this, addressObject);
+
+    if (this.address && !!this.address.city) {
+        this.address.city = addressHelpers.findCurrentCityValue(this.address.city, request.locale);
+    }
+
+    if (this.address && !!this.address.stateCode && this.address.stateCode.length) {
+        this.address.stateCode = addressHelpers.findCurrentStateValue(this.address.stateCode, request.locale);
+    }
+
+
+}
+
 module.exports = address;
