90784b31911609232e9abd14d02d68d61e3ce5e8
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-16 10:00:08 +0200

    Fix new controller

--- cartridges/app_custom_cartridge/cartridge/controllers/New.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/New.js
@@ -4,10 +4,14 @@ var server = require('server');
 
 server.get('Show', function (req, res, next) {
     var address = require('~/cartridge/models/address');
+    var CustomerMgr = require('dw/customer/CustomerMgr');
 
     const demo_prop = 'I am new property';
-    const result = new address({}, demo_prop);
 
+    var customer = CustomerMgr.getCustomerByCustomerNumber('00000001');
+    var rawAddressBook = customer.getAddressBook().getAddress('new_address');
+
+    const result = new address(rawAddressBook, demo_prop);
     res.json({ msg: 'Extended module result', result, req });
     next();
 });
