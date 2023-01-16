909307987b1f4ac7f0c6c0f4ddf952de6da7374c
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-16 10:55:20 +0200

    Validate customer functionality

--- cartridges/app_custom_cartridge/cartridge/controllers/New.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/New.js
@@ -1,18 +1,38 @@
 'use strict';
 
-var server = require('server');
+/**
+ * @namespace New
+ */
 
-server.get('Show', function (req, res, next) {
-    var address = require('~/cartridge/models/address');
-    var CustomerMgr = require('dw/customer/CustomerMgr');
+var server = require('server');
+var userLoggedIn = require('*/cartridge/scripts/middleware/userLoggedIn');
+
+/**
+ * @function
+ * @memberof Error
+ * @param {renders} - json
+ * @param {serverfunction} - get/post
+ */
+server.get('Show', userLoggedIn.validateLoggedIn, function (req, res, next) {
+    const address = require('*/cartridge/models/address');
+    const CustomerMgr = require('dw/customer/CustomerMgr');
 
     const demo_prop = 'I am new property';
 
-    var customer = CustomerMgr.getCustomerByCustomerNumber('00000001');
-    var rawAddressBook = customer.getAddressBook().getAddress('new_address');
+    const customer = CustomerMgr.getCustomerByCustomerNumber('00000001');
 
-    const result = new address(rawAddressBook, demo_prop);
-    res.json({ msg: 'Extended module result', result, req });
+    if (customer) {
+        const rawAddressBook = customer.getAddressBook().getAddress('new_address');
+
+        if (rawAddressBook) {
+            const result = new address(rawAddressBook, demo_prop);
+            return res.json({ msg: 'Extended module result', result, req });
+        }
+
+        return res.json({ msg: 'There is no such address in the customer book', customer, req });
+    }
+
+    res.json({ msg: 'There is no such customer' });
     next();
 });
 
