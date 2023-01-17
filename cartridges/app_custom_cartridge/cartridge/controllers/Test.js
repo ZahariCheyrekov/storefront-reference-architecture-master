c38971ea50c9686f58a34e1add46a1c30551100d
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-17 16:39:28 +0200

    Add test show controller

--- cartridges/app_custom_cartridge/cartridge/controllers/Test.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/Test.js
@@ -7,17 +7,17 @@
 const server = require('server');
 
 /**
+ * Test-Show : This endpoint is called to show the test isml template
+ * @name Test-Show
  * @function
+ * @memberof Test
+ * @param {category} - sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
  */
 server.get('Show', function (req, res, next) {
-    const viewData = res.getViewData();
-
-    viewData.customName = 1;
-    res.setViewData(viewData);
-
     res.render('test');
     next();
 });
-
 
 module.exports = server.exports();
