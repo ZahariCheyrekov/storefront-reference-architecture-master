8a31ec9c87a09a4b43aa0fffa54763091bc80751
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-17 16:58:20 +0200

    Apply jsdocs comments

--- cartridges/app_custom_cartridge/cartridge/controllers/Cart.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/Cart.js
@@ -1,8 +1,21 @@
 'use strict';
 
+/**
+ * @namespace Cart
+ */
+
 var server = require('server');
 server.extend(module.superModule);
 
+/**
+ * Cart-Show : Extends the Cart-Show controller from the app_storefront_base
+ * @name Cart-Show
+ * @function
+ * @memberof Cart
+ * @param {category} - sensitive
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
 server.append('Show', function (req, res, next) {
     res.render('cart/cart');
     next();
