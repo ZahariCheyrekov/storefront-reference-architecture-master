61d960a6898b8864350d16db8d861d154df63ded
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-18 12:24:46 +0200

    Fix controller

--- cartridges/app_custom_cartridge/cartridge/controllers/Custom.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/Custom.js
@@ -6,6 +6,8 @@
 
 const server = require('server');
 
+var pageMetaData = require('*/cartridge/scripts/middleware/pageMetaData');
+
 /**
  * Custom-List : This emdpoint is called to show the search results of the page
  * @name Custom-List
@@ -16,12 +18,18 @@ const server = require('server');
  * @param {serverfunction} - get
  */
 server.get('List', function (req, res, next) {
+    const ProductSearchModel = require('dw/catalog/ProductSearchModel');
+    const results = new ProductSearchModel();
     const query = req.httpParameterMap.query;
 
+    results.setSearchPhrase(query);
+    results.search();
+
     res.render('search/searchResults', {
+        searchResults: results,
         query: query
     });
     next();
-});
+}, pageMetaData.computedPageMetaData);
 
 module.exports = server.exports();
