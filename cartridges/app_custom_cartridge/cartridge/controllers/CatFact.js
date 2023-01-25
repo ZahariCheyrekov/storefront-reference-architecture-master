5a272468361ca447d67f11994c908e9e22aa94aa
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-25 17:58:16 +0200

    Fix show cat fact route

--- cartridges/app_custom_cartridge/cartridge/controllers/CatFact.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/CatFact.js
@@ -12,26 +12,20 @@ const cache = require('*/cartridge/scripts/middleware/cache');
  * @function
  * CatFact-ShowHTTP: Used to return a fact about cats
  * @memberof CatFact
- * @param {middleware} - cache.applyDefaultCache
- * @param {middleware} - server.middleware.include
  * @param {renders} - isml
  * @param {serverfunction} - get
  */
-server.get('Show',
-    server.middleware.include,
-    cache.applyDefaultCache,
-    function (req, res, next) {
-        const httpClient = new dw.net.HTTPClient();
-        httpClient.open('GET', 'https://catfact.ninja/fact');
-        httpClient.send();
+server.get('Show', function (req, res, next) {
+    const httpClient = new dw.net.HTTPClient();
+    httpClient.open('GET', 'https://catfact.ninja/fact');
+    httpClient.send();
 
-        const catFact = JSON.parse(httpClient.text);
+    const catFact = JSON.parse(httpClient.text);
 
-        res.render('catHTTP', {
-            catFact: catFact
-        });
-        next();
-    }
-);
+    res.render('catHTTP', {
+        catFact: catFact
+    });
+    next();
+});
 
 module.exports = server.exports();
