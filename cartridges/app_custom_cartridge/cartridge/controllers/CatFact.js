382ae9e3f25a7dca7bb9af1f0467702da229e983
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-26 14:13:23 +0200

    Implement show service cat fact functionality

--- cartridges/app_custom_cartridge/cartridge/controllers/CatFact.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/CatFact.js
@@ -5,6 +5,7 @@
  */
 
 const server = require('server');
+const catFactService = require('*/cartridge/scripts/catFactService');
 const cache = require('*/cartridge/scripts/middleware/cache');
 
 /**
@@ -23,6 +24,23 @@ server.get('ShowHTTP', function (req, res, next) {
     const catFact = JSON.parse(httpClient.text);
 
     res.render('catHTTP', {
+        catFact: catFact
+    });
+    next();
+});
+
+/**
+ * @name CatFact-ShowService
+ * @function
+ * CatFact-ShowService: Used to return a random fact about cats
+ * @memberof CatFact
+ * @param {renders} - isml
+ * @param {serverfunction} - get
+ */
+server.get('ShowService', function (req, res, next) {
+    const catFact = JSON.parse(catFactService.getCatFact());
+
+    res.render('catService', {
         catFact: catFact
     });
     next();
