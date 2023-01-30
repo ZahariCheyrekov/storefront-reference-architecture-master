70bc159ff3db8a2274e6a3861e3b0f1ba6902bde
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-30 12:47:15 +0200

    Implement demo job step functionality

---
+++ cartridges/app_custom_cartridge/cartridge/scripts/steps/DemoJob.js
@@ -0,0 +1,36 @@
+var CustomObjectMgr = require('dw/object/CustomObjectMgr');
+var { File, FileWriter, XMLStreamWriter } = require('dw/io');
+
+module.exports.execute = function () {
+    var demoObjectIterator = CustomObjectMgr.getAllCustomObjects('DemoObject');
+
+    var xsw;
+    var file;
+    var fileWriter;
+
+    try {
+        file = new File([File.IMPEX, 'src', 'text.xml'].join(File.SEPARATOR));
+        fileWriter = new FileWriter(file);
+
+        xsw = new XMLStreamWriter(fileWriter);
+
+        xsw.writeStartDocument();
+        xsw.writeStartElement('products');
+
+
+        while (demoObjectIterator.hasNext()) {
+            var demo = demoObjectIterator.next();
+            xsw.writeStartElement('product');
+            xsw.writeAttribute('id', demo.custom.product);
+            xsw.writeAttribute('name', demo.custom.name);
+            xsw.writeEndElement();
+        }
+        xsw.writeEndElement();
+
+    } catch (error) {
+
+    } finally {
+        xsw.close();
+        fileWriter.close();
+    }
+}
