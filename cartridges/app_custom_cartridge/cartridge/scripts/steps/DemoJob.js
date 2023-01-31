b49607cf4a17146e5e8c91894859bc584703f2cd
Zahari <zahari.cheyreckov999@gmail.com>
2023-01-31 10:01:53 +0200

    Fix demo job

--- cartridges/app_custom_cartridge/cartridge/scripts/steps/DemoJob.js
+++ cartridges/app_custom_cartridge/cartridge/scripts/steps/DemoJob.js
@@ -1,33 +1,32 @@
-var CustomObjectMgr = require('dw/object/CustomObjectMgr');
-var { File, FileWriter, XMLStreamWriter } = require('dw/io');
+var CustomObjectMgr = require('/dw/object/CustomObjectMgr');
+var File = require("dw/io/File");
+var FileWriter = require("dw/io/FileWriter");
+var XMLStreamWriter = require("dw/io/XMLStreamWriter");
 
 module.exports.execute = function () {
     var demoObjectIterator = CustomObjectMgr.getAllCustomObjects('DemoObject');
-
     var xsw;
-    var file;
     var fileWriter;
 
     try {
-        file = new File([File.IMPEX, 'src', 'text.xml'].join(File.SEPARATOR));
+        var file = new File([File.IMPEX, "src", "text.xml"].join(File.SEPARATOR));
         fileWriter = new FileWriter(file);
 
         xsw = new XMLStreamWriter(fileWriter);
-
         xsw.writeStartDocument();
-        xsw.writeStartElement('products');
 
+        xsw.writeStartElement("products");
 
         while (demoObjectIterator.hasNext()) {
             var demo = demoObjectIterator.next();
-            xsw.writeStartElement('product');
-            xsw.writeAttribute('id', demo.custom.camera);
-            xsw.writeAttribute('name', demo.custom.photo);
+            xsw.writeStartElement("product");
+            xsw.writeAttribute("id", demo.custom.camera);
+            xsw.writeAttribute("name", demo.custom.photo);
             xsw.writeEndElement();
         }
         xsw.writeEndElement();
 
-    } catch (error) {
+    } catch (e) {
 
     } finally {
         xsw.close();
