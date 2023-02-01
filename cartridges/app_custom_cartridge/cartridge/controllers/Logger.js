09d46fd445a8a0cabde6ffe23af24e7ac7a33fd1
Zahari <zahari.cheyreckov999@gmail.com>
2023-02-01 17:58:10 +0200

    Implement logger with array list functionality

--- cartridges/app_custom_cartridge/cartridge/controllers/Logger.js
+++ cartridges/app_custom_cartridge/cartridge/controllers/Logger.js
@@ -21,6 +21,15 @@ server.get('Show', function (req, res, next) {
     const logger = Logger.getLogger(group);
     logger.debug('Test log message for {0}', group);
 
+    const ArrayList = require('dw/util/ArrayList');
+    const arraylist = new ArrayList();
+
+    for (let index = 0; index < 25000; index++) {
+        arraylist.push({
+            test: 'test'
+        });
+    }
+
     res.render('example/exampleA');
     next();
 });
